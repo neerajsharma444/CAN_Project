503b68ca99a9b199e9befd1b061a395e0701eb95
neerajsharma444 <neeraj999456@gmail.com>
2024-03-13 10:10:02 +0530

    Added gifted chat bubble

--- src/screens/Chat/Chat.Styles.js
+++ src/screens/Chat/Chat.Styles.js
@@ -8,66 +8,24 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'space-between',
-  },
-
-  subContainer: {
-    padding: horizontalScale(15), // Use scaling for padding
-  },
-
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
   },
 
   txtInput: {
     flex: 1,
-    paddingLeft: horizontalScale(10), // Use scaling for padding
-    borderWidth: moderateScale(1), // Use scaling for borderWidth
-    borderRadius: moderateScale(10), // Use scaling for borderRadius
+    marginHorizontal: horizontalScale(15),
+    paddingLeft: horizontalScale(10),
+    borderWidth: moderateScale(1),
+    borderRadius: moderateScale(10),
     borderColor: 'rgba(10, 73, 117, 0.37)',
   },
-
+  wrapperStyle: {
+    left: {backgroundColor: '#F0F0F0'},
+    right: {backgroundColor: '#5555FF'},
+  },
   sendBtn: {
     position: 'absolute',
-    right: horizontalScale(15), // Use scaling for positioning
-  },
-
-  chatsContainer: {
-    // backgroundColor: 'red',
-  },
-
-  myMsgContainer: {
-    marginBottom: verticalScale(10), // Use scaling for margin
-    alignItems: 'flex-end',
-  },
-
-  otherMsgContainer: {
-    padding: moderateScale(10), // Use scaling for padding
-    marginBottom: verticalScale(5), // Use scaling for margin
-    borderRadius: moderateScale(10), // Use scaling for borderRadius
-    alignItems: 'flex-start',
-  },
-
-  myTxtContainer: {
-    padding: moderateScale(10), // Use scaling for padding
-    marginBottom: verticalScale(5), // Use scaling for margin
-    borderRadius: moderateScale(10), // Use scaling for borderRadius
-    backgroundColor: 'rgba(10, 73, 117, 0.1)',
-  },
-
-  otherTxtContainer: {
-    padding: moderateScale(10), // Use scaling for padding
-    marginBottom: verticalScale(5), // Use scaling for margin
-    borderRadius: moderateScale(10), // Use scaling for borderRadius
-    backgroundColor: 'rgba(218, 218, 218, 0.38)',
-  },
-
-  message: {
-    fontSize: moderateScale(18), // Use scaling for fontSize
-    color: 'rgba(0,0,0,1)',
-    fontFamily: 'Nunito-Regular',
+    right: horizontalScale(30),
+    bottom: verticalScale(12),
   },
 });
 
--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -1,76 +1,60 @@
-import React, {useState} from 'react';
-import {
-  View,
-  FlatList,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  Image,
-} from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, TouchableOpacity, Image, Text} from 'react-native';
 import Header from '@components/common/Header/Header';
 import IMAGES from '@assets/images';
+import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 import styles from './Chat.Styles';
 
 const Chat = () => {
-  const [newMessage, setNewMessage] = useState('');
-  const [messages, setMessages] = useState([
-    {id: 1, sender: 'Me', message: 'Hi Nitika!', timestamp: '10:00 PM'},
-    {id: 2, sender: 'Nikita', message: 'Hi John!', timestamp: '10:00 PM'},
-  ]);
+  const [messages, setMessages] = useState([]);
 
-  const handleSendMessage = () => {
-    if (newMessage.trim() === '') return; // Don't send empty messages
-    const newId = messages.length + 1;
-    const timestamp = new Date().toLocaleTimeString([], {
-      hour: '2-digit',
-      minute: '2-digit',
-    });
-    const newMessageObj = {
-      id: newId,
-      sender: 'Me',
-      message: newMessage,
-      timestamp,
-    };
-    setMessages([...messages, newMessageObj]);
-    setNewMessage('');
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hey there dev!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((newMessages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, newMessages),
+    );
+  }, []);
+
+  const renderBubble = props => {
+    return <Bubble {...props} wrapperStyle={styles.wrapperStyle} />;
   };
 
-  const renderMessages = ({item}) => {
-    const myMsg = item.sender === 'Me';
+  const renderSend = props => {
     return (
-      <View style={[myMsg ? styles.myMsgContainer : styles.otherMsgContainer]}>
-        <View
-          style={[myMsg ? styles.myTxtContainer : styles.otherTxtContainer]}>
-          <Text style={styles.message}>{item.message}</Text>
-        </View>
-        <Text style={styles.timestamp}>{item.timestamp}</Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => props.onSend({text: props.text.trim()}, true)}
+        style={styles.sendBtn}>
+        <Image source={IMAGES.send} style={styles.sendIcon} />
+      </TouchableOpacity>
     );
   };
 
   return (
     <View style={styles.mainContainer}>
-      <Header renderText="Nitika" renderImage={false} />
-      <View style={styles.subContainer}>
-        <FlatList
-          data={messages}
-          renderItem={renderMessages}
-          keyExtractor={item => item.id.toString()}
-          contentContainerStyle={styles.chatContainer}
-        />
-        <View style={styles.inputContainer}>
-          <TextInput
-            multiline
-            value={newMessage}
-            style={styles.txtInput}
-            onChangeText={setNewMessage}
-            placeholder="Type your message"
-          />
-          <TouchableOpacity style={styles.sendBtn} onPress={handleSendMessage}>
-            <Image source={IMAGES.send} />
-          </TouchableOpacity>
-        </View>
-      </View>
+      <Header text="CAN Admin" image={false} />
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        textInputStyle={styles.txtInput}
+        user={{
+          _id: 1,
+        }}
+        renderBubble={renderBubble}
+        renderSend={renderSend}
+      />
     </View>
   );
 };
